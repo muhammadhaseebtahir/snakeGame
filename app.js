30e864c254c6eb0e65bca304c5ca400b412d0fe7
muhammadhaseebtahir <haseebtahir7860@gmail.com>
2025-11-15 00:45:31 +0500

    working

---
+++ app.js
@@ -0,0 +1,65 @@
+
+const board= document.querySelector(".board")
+const blockHeight= 50
+const blockWidth =50
+
+const cols= Math.floor(board.clientWidth/blockWidth)
+const rows = Math.floor(board.clientHeight/blockHeight)
+
+const blocks=[]
+let intervalId= null
+const direction= "up"
+const snake=[
+    {
+        x:1,y:3
+    },
+    {
+        x:1,y:4
+    },
+    {
+        x:1,y:5
+    }
+]
+
+
+  for(let row=0; row< rows;row++ ){
+  for(let col=0 ;col<cols;col++){
+        const block= document.createElement("div")
+        block.classList.add("block")
+        board.appendChild(block)
+        block.innerText=`${row}-${col}`
+        blocks[`${row}-${col}`]= block
+    }
+}
+
+const render= ()=>{
+    snake.forEach((s)=>{
+        blocks[`${s.x}-${s.y}`].classList.add("fill")
+    })
+}
+
+
+
+
+intervalId= setInterval(()=>{
+let head = null
+
+if(direction === "left"){
+    head= {x: snake[0].x,y: snake[0].y-1}
+} else if(direction === "right"){
+    head= {x: snake[0].x,y: snake[0].y+1}
+} else if(direction === "down"){
+    head= {x: snake[0].x+1,y: snake[0].y}
+}
+else if(direction === "up"){
+    head= {x: snake[0].x-1,y: snake[0].y}
+}
+snake.forEach(s=>{
+    blocks[`${s.x}-${s.y}`].classList.remove("fill")
+})
+snake.unshift(head)
+snake.pop()
+
+render()
+    
+},300)
